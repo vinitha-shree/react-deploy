69ea7e627d31aba17e70959561d5057808abfb5d
vinitha-shree <vinishree.97@gmail.com>
2023-04-13 13:15:56 +0530

    adds deployment script

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return(
         <nav class="navbar navbar-dark bg-success">
             <div className="row col-12 d-flex justify-content-left text-white">
-            <span className="h3">KnowFarmScheme</span>
+            <span className="h3">FUN</span>
             </div>
         </nav>
     )
